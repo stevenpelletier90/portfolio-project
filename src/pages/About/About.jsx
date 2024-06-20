66eb95792b4a4bbe8a1d230e8faf07a1dc61b447
Steven Pelletier <steve.d.pelletier@gmail.com>
2024-06-20 12:42:29 -0400

    about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,8 +4,25 @@ import './About.scss';
 const About = () => {
   return (
     <div className='about'>
-      <h1>About Me</h1>
-      <p>This is the about page.</p>
+      <div className='container'>
+        <div className='profile'>
+          <img src='poo.jpg' alt='Profile' />
+        </div>
+        <div className='content'>
+          <h1>About Me</h1>
+          <p>
+            Hello! I'm Your Name, a passionate web developer with a love for creating beautiful and functional websites.
+          </p>
+          <p>
+            I've been working in the web development industry for several years and have a strong background in both
+            front-end and back-end development.
+          </p>
+          <p>
+            Feel free to browse through my portfolio and contact me if you have any questions or if you’d like to work
+            together.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
