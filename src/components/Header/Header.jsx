9105359d6a813dd15387321caf48ea80a672a7b3
Steven Pelletier <steve.d.pelletier@gmail.com>
2024-06-19 20:05:14 -0400

    changes to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import './Header.scss';
 
 const Header = () => {
   return (
     <header className='header'>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
+      <nav className='nav-container'>
+        <div className='logo'>
+          <Link to='/'>
+            <img src='/path/to/your/logo.png' alt='Logo' />
+          </Link>
+        </div>
+        <ul className='nav-links'>
           <li>
             <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/portfolio'>Portfolio</Link>
           </li>
           <li>
             <Link to='/blog'>Blog</Link>
@@ -19,10 +26,15 @@ const Header = () => {
           <li>
             <Link to='/contact'>Contact</Link>
           </li>
-          <li>
-            <Link to='/portfolio'>Portfolio</Link>
-          </li>
         </ul>
+        <div className='social-media'>
+          <a href='https://www.linkedin.com' target='_blank' rel='noopener noreferrer'>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href='https://www.instagram.com' target='_blank' rel='noopener noreferrer'>
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </div>
       </nav>
     </header>
   );
