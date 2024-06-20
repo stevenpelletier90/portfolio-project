7388bd045b10a3e5716d19d2a180f88561bade6b
Steven Pelletier <steve.d.pelletier@gmail.com>
2024-06-20 09:25:00 -0400

    favicon addition

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.jsx', // Ensure this points to your entry file
@@ -39,6 +40,17 @@ module.exports = {
       filename: '[name].css',
       chunkFilename: '[id].css',
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: '',
+          globOptions: {
+            ignore: ['**/index.html'], // Ignore index.html to avoid conflict
+          },
+        },
+      ],
+    }), // Add this block
   ],
   devServer: {
     static: {
