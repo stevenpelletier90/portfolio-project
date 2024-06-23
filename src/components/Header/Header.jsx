bdd6d28587879c166453a7ca3634069993ac7cd0
Steven Pelletier <steve.d.pelletier@gmail.com>
2024-06-23 17:26:28 -0400

    Header Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,64 +1,14 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { slide as Menu } from 'react-burger-menu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { ImagesContext } from '../../contexts/ImagesContext';
 import './Header.scss';
 
-const styles = {
-  bmBurgerButton: {
-    position: 'fixed',
-    width: '36px',
-    height: '30px',
-    right: '20px',
-    top: '20px',
-  },
-  bmBurgerBars: {
-    background: '#ffffff',
-  },
-  bmCrossButton: {
-    height: '24px',
-    width: '24px',
-    right: '20px',
-    top: '20px',
-  },
-  bmCross: {
-    background: '#bdc3c7',
-  },
-  bmMenuWrap: {
-    position: 'fixed',
-    height: '100%',
-    width: '100%',
-    top: 0,
-  },
-  bmMenu: {
-    background: '#373a47',
-    padding: '2.5em 1.5em 0',
-    fontSize: '1.15em',
-    overflow: 'hidden',
-  },
-  bmItemList: {
-    color: '#b8b7ad',
-    padding: '0.8em',
-    height: 'auto',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  bmItem: {
-    display: 'inline-block',
-  },
-  bmOverlay: {
-    background: 'rgba(0, 0, 0, 0.3)',
-    top: 0,
-    left: 0,
-  },
-};
-
 const Header = () => {
   const images = useContext(ImagesContext);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -69,32 +19,28 @@ const Header = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const handleStateChange = (state) => {
-    setMenuOpen(state.isOpen);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
-  const closeMenu = () => {
-    setMenuOpen(false);
-  };
-
-  const renderNavLinks = () => (
+  const NavLinks = ({ onClick }) => (
     <>
-      <Link to='/about' onClick={closeMenu} className='menu-item'>
+      <Link to='/about' className='nav-link' onClick={onClick}>
         About
       </Link>
-      <Link to='/portfolio' onClick={closeMenu} className='menu-item'>
+      <Link to='/portfolio' className='nav-link' onClick={onClick}>
         Portfolio
       </Link>
-      <Link to='/blog' onClick={closeMenu} className='menu-item'>
+      <Link to='/blog' className='nav-link' onClick={onClick}>
         Blog
       </Link>
-      <Link to='/contact' onClick={closeMenu} className='menu-item'>
+      <Link to='/contact' className='nav-link' onClick={onClick}>
         Contact
       </Link>
     </>
   );
 
-  const renderSocialIcons = () => (
+  const SocialIcons = () => (
     <div className='social-icons'>
       <a href='https://github.com/stevenpelletier90' target='_blank' rel='noopener noreferrer' aria-label='GitHub'>
         <FontAwesomeIcon icon={faGithub} />
@@ -118,26 +64,38 @@ const Header = () => {
       <div className='header-container'>
         <div className='logo'>
           <Link to='/'>
-            <img src={images['logo.png']} alt='Logo of Steven Pelletier' className='custom-logo' />
+            <img src={images['logo.png']} alt='Logo of Steven Pelletier' className='logo-image' />
           </Link>
         </div>
-        {!isMobile && (
-          <>
-            <nav className='nav-links' aria-label='Main navigation'>
-              {renderNavLinks()}
+
+        {isMobile ? (
+          <div className='mobile-menu'>
+            <button
+              onClick={toggleMenu}
+              className={`menu-toggle ${isMenuOpen ? 'open' : ''}`}
+              aria-expanded={isMenuOpen}
+            >
+              <span className='hamburger-icon'></span>
+              <span className='sr-only'>Toggle menu</span>
+            </button>
+          </div>
+        ) : (
+          <div className='desktop-menu'>
+            <nav className='nav-links'>
+              <NavLinks />
             </nav>
-            <div className='social-media-icons'>{renderSocialIcons()}</div>
-          </>
-        )}
-        {isMobile && (
-          <Menu right isOpen={menuOpen} onStateChange={handleStateChange} styles={styles}>
-            <div className='menu-content'>
-              <nav className='mobile-nav-links'>{renderNavLinks()}</nav>
-              {renderSocialIcons()}
-            </div>
-          </Menu>
+            <SocialIcons />
+          </div>
         )}
       </div>
+      {isMobile && (
+        <nav className={`dropdown-menu ${isMenuOpen ? 'open' : ''}`}>
+          <div className='dropdown-content'>
+            <NavLinks onClick={() => setIsMenuOpen(false)} />
+            <SocialIcons />
+          </div>
+        </nav>
+      )}
     </header>
   );
 };
